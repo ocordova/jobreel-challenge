cbb228b673933e94bc9998e332e536cf005576cd
Ós <zeptok@gmail.com>
2024-04-10 16:04:35 -0600

    🎉 Setup Next, Tailwind and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="mx-auto w-full max-w-2xl flex justify-center items-center">
+      <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </h1>
+    </div>
+  );
+}
